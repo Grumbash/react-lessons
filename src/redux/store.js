83607142cf19568665a70e2cd53769ead4aad9a6
Gleb <rossevgleb@gmail.com>
2021-09-29 20:51:33 +0300

    add redux-devtools

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,6 +1,15 @@
-import { createStore } from "redux";
+import { createStore, applyMiddleware } from "redux";
 import reducer from "./reducers";
+import { composeWithDevTools } from "redux-devtools-extension";
 
-const store = createStore(reducer);
+const middleware = [];
+
+const store = createStore(
+  reducer,
+  composeWithDevTools(
+    applyMiddleware(...middleware)
+    // other store enhancers if any
+  )
+);
 
 export default store;
